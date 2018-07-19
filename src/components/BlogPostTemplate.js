8b6194f2879ce845bd8b89da7d3d6c0256358767
Daniel <daniel@stoutlabs.com>
2018-07-19 02:42:45 -0400

    Attempt to fix weird build issue (not happening locally)

--- src/components/BlogPostTemplate.js
+++ src/components/BlogPostTemplate.js
@@ -97,18 +97,3 @@ BlogPostTemplate.propTypes = {
 };
 
 export default BlogPostTemplate;
-
-/*
-<div className="blog-post-container">
-  <div className="blog-post">
-    <h1>{frontmatter.title}</h1>
-    <h2>{frontmatter.date}</h2>
-    <div
-      className="blog-post-content"
-      dangerouslySetInnerHTML={{ __html: html }}
-    />
-    <div className="tags">{frontmatter.tags}</div>
-  </div>
-</div>
-
-*/
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,7 @@
 import React from 'react';
 //import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
+import { graphql, StaticQuery } from 'gatsby';
 import Helmet from 'react-helmet';
-import { StaticQuery } from 'gatsby';
 
 import Header from './Header';
 import Container from './Container';
@@ -12,7 +11,7 @@ import '../assets/styles/index.scss';
 const Layout = ({ children, data }) => (
   <StaticQuery
     query={graphql`
-      query SiteTitleQuery {
+      query TheSiteTitleQuery {
         site {
           siteMetadata {
             title
