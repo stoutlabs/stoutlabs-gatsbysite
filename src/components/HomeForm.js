1077d899ddb4c880eca741f9af48645c1b7ff006
Daniel <daniel@stoutlabs.com>
2018-08-02 22:43:29 -0400

    Forgot to remove the recaptcha import

--- src/components/HomeForm.js
+++ src/components/HomeForm.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Recaptcha from 'react-google-recaptcha';
 import styled from 'styled-components';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
