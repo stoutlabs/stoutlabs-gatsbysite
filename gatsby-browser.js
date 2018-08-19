a1d3b2466c3d64d965d97c9c08f9f03d197d923f
Daniel <daniel@stoutlabs.com>
2018-08-19 12:39:23 -0400

    Remove Google font loading, replace with Typefaces

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+
+require('typeface-emilys-candy');
+require('typeface-merriweather');
