c54ddabef5116a6b8f5f57913a06d57fbc99fdff
D. Stout <daniel@stoutlabs.com>
2019-09-04 13:38:35 -0400

    Adjusting preview call just a bit.

--- src/pages/previews/preview.js
+++ src/pages/previews/preview.js
@@ -5,62 +5,63 @@ import { usePrismicPreview } from "gatsby-source-prismic";
 
 // import { Spinner } from "../components/Spinner";
 
-const PreviewPage = ({ location }) => {
-  const { previewData, path } = usePrismicPreview(location, {
-    repositoryName: "stoutlabs2018"
-  });
-
-  console.log("path: ", path);
-  console.log("previewData: ", previewData);
-
-  const tempPath = "/";
-  useEffect(() => {
-    if (previewData && (tempPath)) {
-      console.log("path here: ", tempPath);
-      console.log("previewData here: ", previewData);
-      window.__PRISMIC_PREVIEW_DATA__ = previewData;
-      navigate(tempPath);
-    }
-  }, [previewData, tempPath]);
-
-  return <div>Loading preview...</div>;
-};
-
 // const PreviewPage = ({ location }) => {
-//   const { allPrismicPage } = useStaticQuery(graphql`
-//     {
-//       allPrismicPage {
-//         nodes {
-//           uid
-//         }
-//       }
-//     }
-//   `);
-//   const pageUIDs = allPrismicPage.nodes.map(node => node.uid);
-
-//   const pathResolver = () => doc => {
-//     const previewedUID = doc.prismicPage.uid;
-
-//     if (pageUIDs.includes(previewedUID)) {
-//       return previewedUID;
-//     } else {
-//       return "/unpublishedPreview";
-//     }
-//   };
-
 //   const { previewData, path } = usePrismicPreview(location, {
-//     repositoryName: "myRepoName",
-//     pathResolver
+//     repositoryName: "stoutlabs2018"
 //   });
 
+//   console.log("path: ", path);
+//   console.log("previewData: ", previewData);
+
+//   const tempPath = "/";
 //   useEffect(() => {
-//     if (previewData && path) {
-//       window.__PRISMIC_PREVIEW_DATA = previewData;
-//       navigate(path);
+//     if (previewData && (tempPath)) {
+//       console.log("path here: ", tempPath);
+//       console.log("previewData here: ", previewData);
+//       window.__PRISMIC_PREVIEW_DATA__ = previewData;
+//       navigate(tempPath);
 //     }
-//   }, [path, previewData]);
+//   }, [previewData, tempPath]);
 
 //   return <div>Loading preview...</div>;
 // };
+
+const PreviewPage = ({ location }) => {
+  const { allPrismicHomepage } = useStaticQuery(graphql`
+    {
+      allPrismicHomepage {
+        nodes {
+          uid
+        }
+      }
+    }
+  `);
+  // const pageUIDs = allPrismicPage.nodes.map(node => node.uid);
+  const pageUIDs = allPrismicHomepage.nodes.map(node => node.uid);
+
+  const pathResolver = () => doc => {
+    const previewedUID = doc.prismicHomepage.uid;
+
+    if (pageUIDs.includes(previewedUID)) {
+      return previewedUID;
+    } else {
+      return "/unpublishedPreview";
+    }
+  };
+
+  const { previewData, path } = usePrismicPreview(location, {
+    repositoryName: "stoutlabs2018",
+    pathResolver
+  });
+
+  useEffect(() => {
+    if (previewData && path) {
+      window.__PRISMIC_PREVIEW_DATA = previewData;
+      navigate(path);
+    }
+  }, [path, previewData]);
+
+  return <div>Loading preview...</div>;
+};
 
 export default PreviewPage;
