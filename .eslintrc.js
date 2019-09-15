26f6a65b42c839c984d842cc27bd33d0e1856a6a
D. Stout <daniel@stoutlabs.com>
2019-09-15 04:38:40 -0400

    Updated eslint config to my own eslint-config-stoutlabs package.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,6 @@
 module.exports = {
-  "extends": ["wesbos"],
-  "rules": {
-    "no-console": [
-      "error", {allow: ["warn", "error"]}
-    ],
-    "quotes": [
-      2,
-      "double",
-      {
-        "avoidEscape": true,
-        "allowTemplateLiterals": true
-      }
-    ],
-    "prettier/prettier": [
-      "error",
-      {
-        "trailingComma": "all",
-        "singleQuote": false,
-        "printWidth": 100
-      }
-    ],
-    "react/prop-types": [
-      1,
-      {
-        "skipUndeclared": true
-      }
-    ],
-    "react/no-danger": [0],
-    "react/state-in-constructor": [0],
-    "react/jsx-props-no-spreading": [0],
-    "react/destructuring-assignment": [1],
-    "no-nested-ternary": [0],
-    "no-plusplus": [0],
-    "camelcase": [0],
-  },
+  "extends": ["stoutlabs"],
+  "rules": {},
   "settings": {
     "import/resolver": {
       "node": {
